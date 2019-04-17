3f28cc1f60080c886c3cf691ed58511461ba130c
Sahil <gargsahil898@gmail.com>
2019-04-17 19:13:14 +0530

    picasso
    Signed-off-by: Sahil <gargsahil898@gmail.com>

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,25 +5,24 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Adapter
 import android.widget.ArrayAdapter
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var student:ArrayList<Student> = ArrayList()
+
+
 
  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        student.add(Student("sahil","garg"))
-
-        var studentadap = StudentAdapter(student,this)
-        lvname.adapter = studentadap
 
 
 
+     Picasso.get().load("https://www.pexels.com/photo/nature-red-love-romantic-67636/").into(img)
+
+ }
 
 
-
-    }
 
 
 
